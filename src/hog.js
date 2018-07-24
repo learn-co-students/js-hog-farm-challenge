256cb4b75dfa9501f8dbeedc0e69e6a344479036
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-07-24 17:27:42 -0400

    Done.

--- src/hog.js
+++ src/hog.js
@@ -1,3 +1,14 @@
 class Hog {
-  
+  constructor(id, name, specialty, greased, medal, weight, img) {
+      this.id = id
+      this.name = name
+      this.specialty = specialty
+      this.greased = greased
+      this.medal = medal
+      this.weight = weight
+      this.img = img
+  }
+  render() {
+    return `<div class="hog-card" id="${this.id}"><h3>${this.name}</h3><p>Specialty: ${this.specialty}</p><p>Medal: ${this.medal}</p><p>Weight: ${this.weight}</p><img src="${this.img}"></img></div>`
+  }
 }
